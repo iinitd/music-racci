9bff1ede82ea1a37a274651de7b85597a00570cc
Cogons <cogons@qq.com>
2017-06-26 22:52:31 +0800

    remove mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var express = require("express");
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var showdown = require('showdown');
 var session = require('express-session');
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var showdown = require('showdown');
 var _ = require('underscore');
